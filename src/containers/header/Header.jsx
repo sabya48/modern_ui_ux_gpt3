f90feb8ed8e4d0592e0d9512a0f17a56e9b9e6d7
Sabyasachi Sarkar <kutusarkar9@gmail.com>
2023-03-05 16:18:41 +0530

    Brand section done

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
             <input type="email" placeholder='Your Email Address' />
             <button type='button'>Get Started</button>
             </div> 
-          <div className='gpt3__header-content-people'>
+          <div className='gpt3__header-content__people'>
             <img src={people} alt="people" />
             <p>1,600 people requested access a visit in last 24 hours</p>
             </div>  
